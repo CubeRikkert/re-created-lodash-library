ba2a46aaca79e4041efe88173e562777f2032109
CubeRikkert <ricksamuel@gmail.com>
2019-02-20 14:41:05 +0100

    second commit

---
+++ _.js
@@ -0,0 +1,18 @@
+const _ = {
+    clamp(number, lower, upper) {
+      return Math.min(Math.max(number, lower), upper)
+    },
+    inRange(number, start, end) {
+      if (end === undefined) {
+        end = start
+        start = 0
+      } if (start > end) {
+        let tempVal = end
+        end = start
+        start = tempVal
+      } 
+      let isInRange = Boolean(number > start && number < end)
+      return isInRange
+    }
+  }
+  
